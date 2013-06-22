4e6d0c4a7077ad8fd77f004b0797e9ea0c135585
lisa ballard <lballard.cat@gmail.com>
2013-06-22 13:56:38 -0700

    scraper refactor

--- scrape_to_wordpress.py
+++ scrape_to_wordpress.py
@@ -13,11 +13,15 @@ from wordpress_xmlrpc.methods import media, posts
 from secrets import *
 
 base_url = 'http://hirise.lpl.arizona.edu/releases/all_captions.php'
-pages = 100
 
 base_url_wallpapers = 'http://hirise.lpl.arizona.edu/'
 local_img_dir = "/Users/lballard/projects/marsfromspace/images/"
 published_url = "https://s3.amazonaws.com/marsfromspace/published.txt"
+
+# get the previously published list
+response = urllib2.urlopen(published_url)
+previously_published = [p.rstrip() for p in response.readlines()]
+
 
 def fetch_remote_file(url, local_img_dir, repeat):
     local_file = local_img_dir + url.split('/')[-1]
@@ -39,8 +43,8 @@ def grab_all_page_urls(base_url):
 
     page_urls = []
     print base_url
-    # for i in range(1,pages+1):
-    for i in range(20, 22):
+    # construct the index page urls from base_url and page no
+    for i in range(80, 70, -1):
         page_urls.append(base_url + '?page=%s' % str(i))
 
     all_links = []
@@ -53,7 +57,7 @@ def grab_all_page_urls(base_url):
 
         try:
             index_soup = BeautifulSoup(index_page)
-            all_cells = index_soup.findAll('td')
+            all_cells = index_soup.findAll('td')  # each listing is in a table cell
             for cell in all_cells:
                 all_links.append(('/').join(base_url.split('/')[:-2]) + '/' + cell.a.get('href').split('/')[1:][0])
         except:
@@ -101,25 +105,30 @@ def grab_content_from_page(detail_url):
     content = content.replace('\n', ' ')  \
                      .replace('href="images/', 'target = "_blank" href="http://hirise.lpl.arizona.edu/images/') \
                      .replace('href="E', 'target = "_blank" href="http://hirise.lpl.arizona.edu/E') \
+                     .replace('href="T', 'target = "_blank" href="http://hirise.lpl.arizona.edu/T') \
+                     .replace('href="r', 'target = "_blank" href="http://hirise.lpl.arizona.edu/r') \
+                     .replace('href="j', 'target = "_blank" href="http://hirise.lpl.arizona.edu/j') \
+                     .replace('href="p', 'target = "_blank" href="http://hirise.lpl.arizona.edu/p') \
+                     .replace('href="d', 'target = "_blank" href="http://hirise.lpl.arizona.edu/d') \
+                     .replace('href="e', 'target = "_blank" href="http://hirise.lpl.arizona.edu/e') \
                      .replace('href="P', 'target = "_blank" href="http://hirise.lpl.arizona.edu/P')
 
     content += '<p>More info and image formats at <a href = "%s">%s</a></p>' % (detail_url, detail_url)
+    content += '<p>Image: NASA/JPL/University of Arizona </p>'
 
     return (title, content, detail_url, local_img_file)
 
 
 def log_as_published(img_id):
     # grab previously published
-    response = urllib2.urlopen(published_url)
-    published = [p.rstrip() for p in response.readlines()]
-    published.append(img_id)
+    previously_published.append(img_id)
     # update the published log
     s3 = boto.connect_s3(AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY)
     bucket = s3.create_bucket(BUCKET_NAME)
     key_name = published_url.split('/').pop()
     bucket.delete_key(key_name)
     key = bucket.new_key(key_name)
-    key.set_contents_from_string("\n".join(published))
+    key.set_contents_from_string("\n".join(previously_published))
     key.set_acl('public-read')
 
 def post_to_wordpress(title, content, detail_url, local_img_file):
@@ -151,9 +160,15 @@ def post_to_wordpress(title, content, detail_url, local_img_file):
 
 all_links = grab_all_page_urls(base_url)
 for detail_url in all_links:
-    print("fetching data from " + detail_url)
-    (title, content, detail_url, local_img_file) = grab_content_from_page(detail_url)
-    print("posting to WP " + title)
-    post_to_wordpress(title, content, detail_url, local_img_file)
+    img_id = detail_url.split('/')[-1]
+    if img_id not in previously_published:
+        print("fetching data from " + detail_url)
+        try:
+            (title, content, detail_url, local_img_file) = grab_content_from_page(detail_url)
+        except TypeError:
+            continue  # move along
+
+        print("posting to WP: " + title)
+        post_to_wordpress(title, content, detail_url, local_img_file)
 
 print("Bye!")
