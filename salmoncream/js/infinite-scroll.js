d71fe67ae92e7eb247bd0826c5f525a69055627b
lisa ballard <lballard.cat@gmail.com>
2013-10-07 22:40:27 -0700

    changes infinite scroll so it triggers when user approaches bottom of document to a percent rather than waiting for user to scroll all the way to bottom

--- salmoncream/js/infinite-scroll.js
+++ salmoncream/js/infinite-scroll.js
@@ -1,9 +1,12 @@
 /*global INFSCR */
+/* hacked by Lisa B */
 
 jQuery(function($) {
 
     var count = 2;
 	var total = INFSCR.max_num_pages;
+    var time_last_load = new Date() / 1000;
+    var the_end_msg = false;
 
 	function loadArticle(pageNumber) {
 		$('#infinite-loader').fadeIn('fast');
@@ -20,15 +23,23 @@ jQuery(function($) {
     }
 
 	$(document).scroll(function() {
-
-		if ( (window.innerHeight + window.scrollY) === $(document).height() ) {
-
+		if ( (window.innerHeight + window.scrollY) >= $(document).height()*0.75) {
 			if (count > total) {
+                if (!the_end_msg) {
+                    the_end_msg = true;
+                    $("<p>The End!</p>").appendTo("#content").hide().fadeIn();
+                }
 				return false;
-			}else{
-				loadArticle(count);
+			}else {
+                time_now = new Date() / 1000;
+                if( ( (time_now - time_last_load) > 5.0) ||
+                    ((window.innerHeight + window.scrollY) === $(document).height()) ) {
+                    // either 10 seconds have gone by or user is sitting at bottom of page
+                    time_last_load = time_now;
+                    loadArticle(count);
+                    count++;
+                }
 			}
-			count++;
 		}
 	});
 
