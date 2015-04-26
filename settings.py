f4663108dd5546be9d3b177cb2d5e7a1f48527cc
Lisa Ballard <lballard.cat@gmail.com>
2015-04-26 16:19:59 -0700

    corrected some nonsense

--- settings.py
+++ settings.py
@@ -1,8 +1,6 @@
-WP_USER = S3Client(os.environ['WP_USER'])
-WP_PW = S3Client(os.environ['WP_PW'])
-BUCKET_NAME = S3Client(os.environ['BUCKET_NAME'])
-AWS_ACCESS_KEY_ID = S3Client(os.environ['AWS_ACCESS_KEY_ID'])
-AWS_SECRET_ACCESS_KEY = S3Client(os.environ['AWS_SECRET_ACCESS_KEY'])
-
-
+WP_USER = os.environ['WP_USER']
+WP_PW = os.environ['WP_PW']
+BUCKET_NAME = os.environ['BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
