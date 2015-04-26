a5051ee9911eee6ace6ce57f612df9f9ffc51f8e
Lisa Ballard <lballard.cat@gmail.com>
2015-04-26 15:40:54 -0700

    adds settings.py file - fetches heroku config vars

---
+++ settings.py
@@ -0,0 +1,8 @@
+WP_USER = S3Client(os.environ['WP_USER'])
+WP_PW = S3Client(os.environ['WP_PW'])
+BUCKET_NAME = S3Client(os.environ['BUCKET_NAME'])
+AWS_ACCESS_KEY_ID = S3Client(os.environ['AWS_ACCESS_KEY_ID'])
+AWS_SECRET_ACCESS_KEY = S3Client(os.environ['AWS_SECRET_ACCESS_KEY'])
+
+
+
